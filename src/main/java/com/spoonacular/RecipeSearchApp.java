ec574af67c0854554d4459296ed9bdeb15d729b9
DaanveerH <71367299+DaanveerH@users.noreply.github.com>
2024-03-10 11:31:42 -0700

    Api call
    Got the API call to work from the spoonacular docs. Trying to replicate for my other machine. You can pull from this branch to try it out yourself.
    
    Further goals here is to get this to implement with the Matthew-weiss repo. And store into MongoDB

---
+++ src/main/java/com/spoonacular/RecipeSearchApp.java
@@ -0,0 +1,53 @@
+package com.spoonacular;
+
+import com.spoonacular.client.ApiClient;
+import com.spoonacular.client.ApiException;
+import com.spoonacular.client.Configuration;
+import com.spoonacular.client.auth.ApiKeyAuth;
+import com.spoonacular.DefaultApi;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class RecipeSearchApp {
+    private static final String API_KEY = "41a61e7ce95149b2975318bec5ac0783";
+
+    public static void main(String[] args) {
+        String baseUrl = "https://api.spoonacular.com/recipes/complexSearch";
+        String query = "pasta";
+        int maxFat = 25;
+        int number = 2;
+
+        // Construct the query URL
+        String urlString = String.format("%s?query=%s&maxFat=%d&number=%d&apiKey=%s",
+                baseUrl, query, maxFat, number, API_KEY);
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            // Check for successful response
+            
+            int responseCode = conn.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                System.out.println("HTTP request failed with code: " + responseCode);
+                return;
+            }
+
+            // Read the response
+            Scanner scanner = new Scanner(url.openStream(), StandardCharsets.UTF_8.toString());
+            scanner.useDelimiter("\\Z");
+            String response = scanner.next();
+            scanner.close();
+
+            System.out.println("Response: " + response);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
